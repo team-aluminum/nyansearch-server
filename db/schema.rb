a277438a9ce498bcb3542b2682b4da952467b8ca
yamamoto kenta <yamamoto7.pgming@gmail.com>
2019-06-08 22:56:35 +0900

    [create: table] cat

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_08_135017) do
+ActiveRecord::Schema.define(version: 2019_06_08_135441) do
+
+  create_table "cats", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "shops", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name"
