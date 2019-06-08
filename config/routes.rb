4daf6a878ce6a18e577236429e1993c029890eee
yamamoto kenta <yamamoto7.pgming@gmail.com>
2019-06-09 07:42:13 +0900

    [change: path name] of shops#get_sound

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :shops, only: [:index, :show], defaults: { format: :json }
-  get '/get_sound/:direction/:longitude/:latitude' => 'shops#get_sound', defaults: { format: :json }
+  get '/sound/:direction/:longitude/:latitude' => 'shops#get_sound', defaults: { format: :json }
 end
