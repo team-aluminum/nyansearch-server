bc6036226c098004eb0aa2e3a86fa789bee8d7c5
yamamoto kenta <yamamoto7.pgming@gmail.com>
2019-06-09 06:41:07 +0900

    [create: action] for calculate cat's direction

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,3 +1,4 @@
+include Math
 class ShopsController < ApplicationController
   def index
     @shops = Shop.get_shops_from_cord
@@ -5,5 +6,49 @@ class ShopsController < ApplicationController
 
   def show
     @shop = Shop.find(params[:id])
+  end
+  def get_sound
+    @shop = Shop.find(1)
+    # a = Shop.find(3)
+    # b = Shop.find(2)
+    @ang = get_angle(111,100,180,180)
+    # @ang = get_angle(a.longitude.to_f, a.latitude.to_f, b.longitude.to_f, b.latitude.to_f)
+    @dir = get_user_dir(@ang)
+    # @dis = get_distance(a.longitude.to_f, a.latitude.to_f, b.longitude.to_f, b.latitude.to_f)
+    @url = "https://aluminium-cats.s3-ap-northeast-1.amazonaws.com/cat#{@shop.id % 3 + 1}_1_#{@dir}.mp3"
+  end
+
+  private
+  def get_distance(lng1, lat1, lng2, lat2)
+    x1 = lat1.to_f * Math::PI / 180
+    y1 = lng1.to_f * Math::PI / 180
+    x2 = lat2.to_f * Math::PI / 180
+    y2 = lng2.to_f * Math::PI / 180
+    
+    radius = 6378.137
+    diff_y = (y1 - y2).abs
+    
+    calc1 = Math.cos(x2) * Math.sin(diff_y)
+    calc2 = Math.cos(x1) * Math.sin(x2) - Math.sin(x1) * Math.cos(x2) * Math.cos(diff_y)
+    
+    numerator = Math.sqrt(calc1 ** 2 + calc2 ** 2)
+    denominator = Math.sin(x1) * Math.sin(x2) + Math.cos(x1) * Math.cos(x2) * Math.cos(diff_y)
+    degree = Math.atan2(numerator, denominator)
+    return degree * radius
+  end
+
+  def get_angle(lng1, lat1, lng2, lat2)
+    y = cos(lng2 * PI / 180) * sin(lat2 * PI / 180 - lat1 * PI / 180)
+    x = cos(lng1 * PI / 180) * sin(lng2 * PI / 180) - sin(lng1 * PI / 180) * cos(lng2 * PI / 180) * cos(lat2 * PI / 180 - lat1 * PI / 180)
+    dirE0 = 180 * atan2(y, x) / PI
+    if (dirE0 < 0) then
+      dirE0 = dirE0 + 360
+    end
+    dirN0 = (dirE0 + 90) % 360
+    return dirN0
+  end
+
+  def get_user_dir(angle)
+     return (angle * 8 / 360).round
   end
 end
