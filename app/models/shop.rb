bada57a6305ee22bf086fdafca84aa2f721e91a7
yamamoto kenta <yamamoto7.pgming@gmail.com>
2019-06-09 01:54:53 +0900

    [fix: typo]

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -3,7 +3,7 @@ class Shop < ApplicationRecord
 
   LONGTITUDE_DIFF = 30000
   LATITUDE_DIFF = 30000
-  def self.get_cats_from_cord(lon = 0, lat = 0)
+  def self.get_shops_from_cord(lon = 0, lat = 0)
     self.where(['? < longitude < ? and ? < latitude < ?', (lon - LONGTITUDE_DIFF), (lon + LONGTITUDE_DIFF), (lat - LATITUDE_DIFF), (lat + LATITUDE_DIFF)])
   end
 end
