8fc35423384badc722656eb96f404ebe9dea5b79
yamamoto kenta <yamamoto7.pgming@gmail.com>
2019-06-09 10:02:36 +0900

    [merge: actions] merge index into get_sound

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,21 +1,14 @@
 class ShopsController < ApplicationController
-  def index
-    @shops = Shop.get_shops_from_cord
-  end
-
-  def show
-    @shop = Shop.find(params[:id])
-  end
-
   def get_sound
+    @shops = Shop.get_shops_from_cord
     shop = Shop.get_shops_from_cord.get_nearest_shop(params[:longitude], params[:latitude])
     ang = shop.get_angle(params[:longitudea].to_f,params[:latitude].to_f)
-    dir = get_user_dir(ang)
+    dir = get_user_dir(ang, params[:direction])
     @url = "https://aluminium-cats.s3-ap-northeast-1.amazonaws.com/cat#{shop.id % 3 + 1}_1_#{dir}.mp3"
   end
 
   private
-  def get_user_dir(angle)
+  def get_user_dir(angle, user_direction)
      return (angle * 8 / 360).round
   end
 end
--- app/views/shops/get_sound.json.jbuilder
+++ app/views/shops/get_sound.json.jbuilder
@@ -1,1 +1,12 @@
 json.url @url
+json.shops do
+  json.array! @shops do |shop|
+    json.extract! shop, :id, :address, :longitude, :latitude
+    json.cats do
+      json.array! shop.cats do |cat|
+        json.extract! cat, :id, :name
+      end
+    end
+  end
+end
+
